b0ed75974cc759d32d9ff042d032b2eb358a07bb
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-03 15:52:35 +0200

    modif gitignore

---
+++ tmp.ps1
@@ -0,0 +1,7 @@
+echo "# 12X001-Algos-TPs" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin git@github.com:David-Kyrat/12X001-Algos-TPs.git
+git push -u origin master
