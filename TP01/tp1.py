be3d53b62ab339816ef5492a72c6f92bc0122ebf
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-06 13:44:49 +0200

    TP01 Passed all tests for reduce and is_majority

--- TP01/tp1.py
+++ TP01/tp1.py
@@ -1,4 +1,5 @@
 from math import floor, ceil
+from random import randint
 
 ########################### Exercise 1 ###########################
 
@@ -22,9 +23,8 @@ def is_majority(A, element):
     n = len(A)
     if n == 0: return False 
     if n == 1: return element == A[0]
-    
-    anc = n // 2 + 1 #ac := appearence number constraint. number of times it must appear to be the majority element
-    i, crt_an = 0, 0 # current appearence number
+    anc = n // 2 + 1 #* anc := appearence number constraint. number of times it must appear to be the majority element
+    i, crt_an = 0, 0 #* current appearence number
     for el in A:
         if el == element:
             crt_an += 1
@@ -36,13 +36,21 @@ def reduce(A):
     """Reduces A in at most len(A) // 2 parts using pair-wise votes"""
     n = len(A)
     if n <= 1: return A #* If A is empty or a singleton => result will be A itself
+
+    if n % 2 == 1:
+       rand_el = A[randint(0, n-1)]
+       if is_majority(A, rand_el): return rand_el
+       else: A.remove(rand_el) # Now n is even
+
     A_prime = []
     for i in range(0, n-1, 2):
       a, b = A[i], A[i+1]
       if a == b: A_prime.append(a)
     return A_prime
         
-    
+def handle_reduce_odd(A):
+     n = len(A)
+     
     
 def dandc(A):
     """Divide and Conquer algorithm"""
