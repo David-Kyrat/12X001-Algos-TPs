e07872c65fb20107d11524c57823bbf4261dcaf2
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-11-27 18:35:37 +0100

    added Tp04

---
+++ TP04/test_tp4.py
@@ -0,0 +1,49 @@
+########################### Exercise 1 ###########################
+from tp4 import get_solution
+def test_get_solution():
+    M = [
+        [0,None,None,None,None,None,None,None],
+        [4.9,0,None,None,None,None,None,None],
+        [6,3,0,None,None,None,None,None],
+        [7.6,4.5,2.8,0,None,None,None,None],
+        [11.4,9.3,8.3,6.5,0,None,None,None],
+        [14,11.4,11.1,9.3,3.7,0,None,None],
+        [16.5,14,13.9,12.9,7.4,5.6,0,None],
+        [21,19,13.9,13.9,13.9,12,7.4,0]
+    ]
+    
+    T = [
+        [0, None, None, None, None, None, None, None],
+        [4.9, 0, None, None, None, None, None, None],
+        [6, 3, 0, None, None, None, None, None],
+        [7.6, 4.5, 2.8, 0, None, None, None, None],
+        [11.4, 9.3, 8.3, 6.5, 0, None, None, None],
+        [14, 11.4, 11.1, 9.3, 3.7, 0, None, None],
+        [16.5, 14, 13.9, 12.9, 7.4, 5.6, 0, None],
+        [19.9, 16.9, 13.9, 13.9, 13.9, 12, 7.4, 0]
+    ]
+    
+    count, path = 19.9, [0, 2, 7]
+    assert get_solution(M) == (T, count, path)
+
+
+########################### Exercise 2 ###########################
+from tp4 import compute_change
+
+def test_compute_change():
+    coin_set = [30, 24, 12, 6, 3, 1]
+    
+    assert compute_change(1, coin_set) == [1]
+    assert compute_change(30, coin_set) == [30]
+    assert compute_change(48, coin_set) == [24, 24] # Optimal solution greedy could not find.
+
+########################### Exercise 3 ###########################
+from tp4 import longest_common_subsequence, longest_common_substring
+
+def test_longest_common_subsequence():
+    a, b = ["a", "c", "t", "g", "a", "a"], ["c", "g", "a", "t"]
+    assert longest_common_subsequence(a, b) == ["c", "g", "a"]
+
+def test_longest_common_substring():
+    a, b = ["a", "c", "t", "g", "a", "a"], ["c", "g", "a", "t"]
+    assert longest_common_substring(a, b) == ["g", "a"]
---
+++ TP04/tp4.py
@@ -0,0 +1,24 @@
+########################### Exercise 1 ###########################
+def get_solution(M):
+    """Returns a table T where each value T_ij tells the minimum cost to get from city i to city j based on the cost matrix M.
+    Also returns the cost of travelling from city 0 to n-1 and the respective path as a list of indexes."""
+    # TODO
+    ...
+
+    
+########################### Exercise 2 ###########################
+def compute_change(money, coin_set):
+    """Returns the optimal change (lowest number of coins) for the given 'money' and 'coin set'."""
+    # TODO
+    ...
+
+########################### Exercise 3 ###########################
+def longest_common_subsequence(A, B):
+    """Returns the longest common subsequence (LCS) between lists A and B"""
+    # TODO
+    ...
+
+def longest_common_substring(A, B):
+    """Returns the longest common substring between lists A and B"""
+    # TODO
+    ...
