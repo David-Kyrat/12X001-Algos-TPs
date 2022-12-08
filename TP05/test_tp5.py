c4c3af74ac180f6e152826e18188852d94cb05df
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-08 09:25:21 +0200

    added TP05

---
+++ TP05/test_tp5.py
@@ -0,0 +1,56 @@
+########################### Exercise 1 ###########################
+from tp5 import c_hat, solve_taquin
+from tp5 import UP, RIGHT, DOWN, LEFT
+
+board = [
+        [1, 2, 3, 4],
+        [5, 6, 16, 8],
+        [9, 10, 7, 11],
+        [13, 14, 15, 12]
+    ]
+
+def test_c_hat():
+    assert c_hat(board) == 3
+
+
+def test_solve_taquin():
+    assert solve_taquin(board) == [DOWN, RIGHT, DOWN]
+
+########################### Exercise 2 ###########################
+from tp5 import solve_tasks
+
+def test_solve_tasks():
+    cost_matrix = [
+    [11, 14, 11, 17],
+    [12, 15, 17, 14],
+    [18, 13, 19, 20],
+    [40, 22, 23, 28]
+    ]
+    
+    assert solve_tasks(cost_matrix) == [0, 3, 1, 2]
+
+
+########################### Exercise 3 ###########################
+from tp5 import solve_shortest_path
+
+def test_solve_shortest_path():
+    domain = [
+                    # We want to get here      
+                            # |
+                            # v
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+                         # ^
+                         # |
+                # We start from here
+    ]
+    a, b = (7, 6), (0, 7)
+    solution = [a, (7, 7), (6, 7), (5, 7), (4, 7), (3, 7), (2, 7), (1, 7), b]
+    
+    assert solve_shortest_path(domain, a, b) == solution
---
+++ TP05/tp5.py
@@ -0,0 +1,31 @@
+########################### Exercise 1 ###########################
+# Definitions
+UP, RIGHT, DOWN, LEFT = "up", "right", "down", "left"
+
+def c_hat(board):
+    """Cost function for a given E-Node 'x'.
+    The cost of a node of the game is the number of squares that are not at their place (16 excluded)."""
+    # TODO
+    ...
+
+def solve_taquin(board):
+    """Returns a list of actions to be done to solve the game of Taquin from the given starting solution."""
+    # TODO
+    ...
+
+    
+
+########################### Exercise 2 ###########################
+def solve_tasks(cost_matrix):
+    """Finds the best combination of agents and tasks given the cost matrix.
+    Returns the answer as a list of task numbers for each agent (in order of agents)."""
+    # TODO
+    ...
+    
+
+########################### Exercise 3 ###########################
+def solve_shortest_path(domain, a, b):
+    """Finds the shortest path from point a to point b according to the 2-dimensional domain.
+    The path is returned as a list of steps from a to b, where each step is a tuple with 2 integers."""
+    # TODO
+    ...
