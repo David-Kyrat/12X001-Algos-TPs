54afb76232636fdb20f107ad177255d5abef2841
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-10 23:01:52 +0100

    testing swap

--- TP05/fifteen_puzzle.py
+++ TP05/fifteen_puzzle.py
@@ -7,10 +7,10 @@ DIM = 4  # Dimension of the board
 
 
 class M(Enum):
-    UP = (0, -1)
-    DOWN = (0, 1)
-    RIGHT = (1, 0)
-    LEFT = (-1, 0)
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    RIGHT = (0, 1)
+    LEFT = (0, -1)
 
     v = property(lambda self: self.value)
     # e.g. M.UP() returns (0, -1)
@@ -32,6 +32,45 @@ class M(Enum):
 
     def inv(self): return M((-self()[0], -self()[1]))
 
+def isMisplaced(board: list[list[int]], coord:tuple[int, int]) -> bool:
+    """Returns True if the element at coord is misplaced, False otherwise. i.e. 15 should be at (3, 3)"""
+    return board[coord[0]][coord[1]] == 4 * coord[0] + coord[1] + 1
+
+
+def tmp():
+    import numpy
+    B = numpy.dstack(numpy.indices((4,4)))
+    return [list(map(lambda x: 4*x[0]+x[1]+1, B[i])) for i in range(len(B))]
+
+B = tmp()
+
+def swap(board: list[list[int]], tx:tuple[int, int], move: M, misplaced: set[tuple[int, int]]) -> set[tuple[int, int]]:
+    """Swap index and index coord in coord in the board.
+
+    Parameters
+    ----------
+    @ `board` - Game board
+    @ `tx` - index of white square (16)
+    @ `move` - move applied to the white square
+    @ `misplaced` - set of misplaced tiles
+
+    Returns
+    ----------
+        `misplaced` - updated set of misplaced tiles
+    """
+    nc = move + tx # new coord
+    board[tx[0]][tx[1]], board[nc[0]][nc[1]] = board[nc[0]][nc[1]], board[tx[0]][tx[1]] # swap
+
+    print(isMisplaced(board, nc), nc)
+    # if the new coord is misplaced, add it to the misplaced set
+    if isMisplaced(board, nc): misplaced.add(nc)
+    
+    print(isMisplaced(board, tx), tx)
+    # if the swap corrected the position of a tile, remove it from the misplaced set
+    if not isMisplaced(board, tx) and tx in misplaced: 
+        misplaced.remove(tx)
+    
+    
 
 class Node: 
     """Node of the game tree. Each node has a ``depth`` (``int``), a cost (``int``), a list of moves ``moves`` (``list[M]``) 
@@ -56,7 +95,7 @@ class Node:
         self.depth = parent.depth + 1
         self.cost = cost
         self.moves = parent.moves + [move]
-        self.tx = move + parent.tx # addition between a move and a tuple was defined in the M class above. e.g. ``M.UP + (3, 2)`` returns ``(3, 1)``
+        self.tx = move + parent.tx # addition between a move and a tuple was defined in the M class above. e.g. ``M.UP + (3, 2)`` returns ``(2, 2)``
         self.gx = cost - (parent.depth + 1)
 
     def __init_root__(self, cost:int, taquin_index: tuple[int, int]):
@@ -94,3 +133,5 @@ def solve_taquin(board: list[list[int]]) -> list[str]:
     ----------
     @ `board` - board to solve
     """
+
+
