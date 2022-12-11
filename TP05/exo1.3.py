284363eb9d85e93910195f94f1413db8e2109e41
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-11 13:08:06 +0100

    gen_disored seems to work

--- TP05/exo1.3.py
+++ TP05/exo1.3.py
@@ -28,7 +28,9 @@ def children_move_fromList(move:M, tx:tuple[int, int], DIM:int) -> set[M]:
     if tx[1] == 0: moves.discard(M.LEFT)
     if tx[1] == DIM-1: moves.discard(M.RIGHT)
 
-def gen_disorder(board: list[list[int]], node:Node, n):
+    return moves
+
+def gen_disorder(board: list[list[int]], node:Node, n:int):
     """Creates disorder in the board by moving (at random) the blank space n times. (Usually ``node`` is a goal node but need not necessarily be.)
     NB: the moves are selected at random but they are still filtered to try to avoid cycles, and out of bounds exceptions.
     (I.e. the move is selected at random among the result of a modified version ``children_moves()``)
@@ -45,17 +47,30 @@ def gen_disorder(board: list[list[int]], node:Node, n):
 
     Returns
     ----------
-        (``tx_final``, ``misplaced_final``)  - Pair containing the final position of the white square and the final set of misplaced tiles (after the n random moves).
+        ``(tx_final, misplaced_final)``  - Pair containing the final position of the white square and the final set of misplaced tiles (after the n random moves).
     """
     misplaced: set[tuple[int, int]] = node.misplaced.copy() # empty if node is a goal node
     
     DIM, tx = len(board), apply_moves(board, node.tx0, node.moves, misplaced)
-
+    move: M = random.choice(list(children_move_fromList(None, tx, DIM)))
     for _ in range(n):
         move = random.choice(list(children_move_fromList(move, tx, DIM)))
         tx = apply_moves(board, tx, [move], misplaced)
 
     return tx, misplaced
+
+#! Since printing the board did not seem like a fundamental task of this TP, The following function to print the board in color was taken from internet.
+def printBoard(board:list[list[int]], DIM:int, tx:tuple[int, int], misplaced:set[tuple[int, int]]):
+    """Prints the board with the white square (16) and the misplaced tiles highlighted."""
+    for i in range(DIM):
+        for j in range(DIM):
+            if (i, j) == tx:
+                print(f"{board[i][j]:2d} ", end="")
+            elif (i, j) in misplaced:
+                print(f"\033[1;31m{board[i][j]:2d}\033[0m ", end="")
+            else:
+                print(f"{board[i][j]:2d} ", end="")
+        print()
 
 if __name__ == '__main__':    
     board = [[1, 2, 3, 4], 
@@ -63,7 +78,12 @@ if __name__ == '__main__':
             [9, 10, 7, 11],
             [13, 14, 15, 12]]
 
-    solution = solve_taquin(board)
+    goalNode:Node = solve_taquin(board, extract_path_from_goalNode=False)
+    print(goalNode)
 
-    print(so)
+    n:int = 5
+    final_tx, misplaced = gen_disorder(board, goalNode, n)
+    print([board[p[0]][p[1]] for p in misplaced])
+    printBoard(board, len(board), final_tx, misplaced)
 
+    
