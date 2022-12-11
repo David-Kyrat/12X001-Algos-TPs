73bf7d6d91456fe891fdacdda931d116bea1815b
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-11 15:18:46 +0100

    added util.py

---
+++ TP05/util.py
@@ -0,0 +1,81 @@
+from timeit import timeit
+import matplotlib.pyplot as plt
+from time import time, time_ns
+
+
+def plotVS(plot_x, plot_f1, plot_f2, title: str, xlabel: str, ylabel: str, f1Label: str, f2Label: str):
+    """Plots two functions against each other.
+    - plot_x: the x-axis values
+    - plot_f1: the y-values of the first function
+    - plot_f2: the y-values of the second function
+    - title: the title of the plot
+    - xlabel: the label of the x-axis
+    - ylabel: the label of the y-axis
+    - f1Label: the label of the first function
+    - f2Label: the label of the second function
+    """
+
+    plt.title(title)
+    plt.grid()
+    m1, m2, gap = min(plot_f2), max(plot_f1), 0.2
+    plt.ylim(m1, m2 - gap * m2)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(plot_x)
+    plt.plot(plot_x, plot_f1, '-k', label=f1Label, linewidth=1)
+    plt.plot(plot_x, plot_f2, '-r', label=f2Label, linewidth=3)
+    
+    #plt.legend(prop={'size': 5})
+    #plt.legend(fontsize=7)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_solo(plot_x, plot_f, title: str, xlabel: str, ylabel: str, fLabel: str):
+    """Plots a single function
+    - plot_x: the x-axis values
+    - plot_f: the y-values of the function
+    - title: the title of the plot
+    - xlabel: the label of the x-axis
+    - ylabel: the label of the y-axis
+    - fLabel: the label of the function
+    """
+    #plt.tight_layout(pad=5)
+    fig = plt.figure()
+    plt.title(title)
+    plt.grid()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    M, gap = max(plot_f), 0.25
+    plt.xticks(plot_x)
+    plt.ylim(0, M + gap*M)
+    #plt.yticks(np.linspace(0, M, 27))
+    plt.plot(plot_x, plot_f, '-b', label=fLabel, linewidth=1)
+    #  plt.legend(prop={'size': 5})
+    #  plt.legend(fontsize=7)
+    fig.set_figheight(7)
+    plt.legend()
+    plt.show()
+
+
+def runtime(f, args, unpack: bool):
+    '''It takes a function f and an argument arg, and returns the time (in ns) it takes to run f(arg)
+    
+    Parameters
+    ----------
+    @ f - the function to be timed
+    @ args - the arguments to pass to the function
+    @ unpack - bool, optional if True, the function will unpack the arguments 
+    before passing them to the function.
+    
+    Returns
+    -------
+        The time it takes to run the function f with the arguments args'''
+    
+    delta_time = timeit(lambda : f(*args) if (unpack) else f(args), number=1) #* 1.000    
+    return delta_time
+
+def runtime_arr(f, argArray, unpack: bool): 
+    """ See "runtime()" function above for explaination """
+    return [runtime(f, arg, unpack) for arg in argArray]
