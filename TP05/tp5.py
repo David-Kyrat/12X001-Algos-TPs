f9ce42d8f10c02a4d4fbf1235c13b7641c5c624a
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-12 00:32:51 +0100

    TP05 -- Exo 1 & 3 -- All Test Passed

--- TP05/tp5.py
+++
@@ -1,7 +0,0 @@
-########################### Exercise 2 ###########################
-def solve_tasks(cost_matrix):
-    """Finds the best combination of agents and tasks given the cost matrix.
-    Returns the answer as a list of task numbers for each agent (in order of agents)."""
-    # TODO
-    ...
-
