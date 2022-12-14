49a88cbc58c723bb892b2d2d0684cbaef7952d54
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-14 16:28:56 +0100

    added TP06

---
+++ TP06/exo1.py
@@ -0,0 +1,8 @@
+########################### Exercise 1 ###########################
+def approx_vertex_cover(matrix):
+    """Returns an approximation for the vertex-cover problem.
+    The approximation is at most 2-times worse than the optimal solution"""
+    # TODO
+    pass
+
+    
