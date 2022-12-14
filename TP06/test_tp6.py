298cc9f86c12ea3d6bad897b7128639f9554d902
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-14 16:26:02 +0100

    added TP06

---
+++ TP06/test_tp6.py
@@ -0,0 +1,47 @@
+########################### Exercise 1 ###########################
+from tp6 import approx_vertex_cover
+
+def test_approx_vertex_cover():
+    # In case of a fully connected graph, the algorithm simply returns all nodes in order
+    n = 10
+    matrix = [[1 if i != j else 0 for j in range(n)] for i in range(n)]
+    assert approx_vertex_cover(matrix) == list(range(n))
+    
+    # Example from the slides
+    matrix = [
+        [0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 1, 1, 1],
+        [0, 0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+    ]
+    assert approx_vertex_cover(matrix) == [0, 1, 2, 3, 4, 5]
+    
+    # Another example
+    matrix = [
+        [0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0]
+    ]
+
+    assert approx_vertex_cover(matrix) == [0, 1]
+    
+    
+
+
+########################### Exercise 2 ###########################
+from tp6 import approx_knapsack
+
+def test_approx_knapsack():
+    w, v = [12, 10, 7, 3, 1], [9, 5, 4, 2, 2]
+    assert approx_knapsack(w, v, 0) == ([], [])
+    assert approx_knapsack(w, v, 10) == ([7, 1], [4, 2])
+    assert approx_knapsack(w, v, 12) == ([12], [9])
+    assert approx_knapsack(w, v, 22) == ([12, 1, 3], [9, 2, 2])
+    assert approx_knapsack(w, v, 29) == ([12, 1, 3, 7], [9, 2, 2, 4])
---
+++ TP06/tp6.py
@@ -0,0 +1,14 @@
+########################### Exercise 1 ###########################
+def approx_vertex_cover(matrix):
+    """Returns an approximation for the vertex-cover problem.
+    The approximation is at most 2-times worse than the optimal solution"""
+    # TODO
+    pass
+
+    
+########################### Exercise 2 ###########################
+def approx_knapsack(weights, values, max_weight):
+    """Returns an approximation for the (0-1) knapsack problem.
+    The approximation is at most 2-times worse than the optimal solution"""
+    # TODO
+    pass
