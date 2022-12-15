d48911137760800b6892eb3152f6f4ce2b2bf6d4
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-12-15 11:01:52 +0100

    Cleanup exo2

--- TP06/exo2.py
+++ TP06/exo2.py
@@ -23,26 +23,7 @@ class El:
     def get(self, i): return [] if i >= len(self.idxs) else self.idxs[i]
     # useful because we're not going to store a solution as a list but instead as one element that will contain the sum of each values and weight and the list of indexes
     def __add__(self, other): return El((self.get(0) + other.get(0), self.get(1) + other.get(1)), self.w + other.w, self.v + other.v)
-
-    def __eq__(self, __o: object) -> bool:
-        if not isinstance(__o, El): return False
-        return self.v == __o.v and self.w == __o.w
-
-    def __lt__(self, __o: object) -> bool:
-        """ El_1 < El_2 if and only if El_2 has a greater value than El_1.
-            i.e. the max of a list of El is the one with the greatest value"""
-        if not isinstance(__o, El): raise ValueError("Cannot compare El with other types")
-        return self.v < __o.v
     
-    # to be able to add an El in a set
-    def __hash__(self) -> int: return hash((self.w, self.v))
-    
-#def sort_and_createEls(weights, values):
-    """Sorts the weights and values in decreasing order of the ratio (value / weight).
-    Returns a list of El instances"""
-    # We sort the values and the corresponding weights in decreasing order
-    
-
 def greedy_knapsack(items: list, W, b0:El=El([],0,0)) -> El:
     """:return: Solution of Knapsack problem for set of item "items" and max weight W, by selecting first
         all the items with the highest <characteristic defined by sorting key>
@@ -69,43 +50,16 @@ def approx_knapsack(weights, values, max_weight) -> tuple:
     # We sort the values and the corresponding weights in decreasing order
     if max_weight <= 0: return [], []
     E: list[El] = [El(([weights[i]], [values[i]]), weights[i], values[i]) for i in range(len(weights))]
-    #E.sort(key=lambda e: e.v)
-    R = set()
+    R = []
     for j, el in enumerate(E):
-        #crt:El = El([], 0, 0)
-        #if E[j].w <= max_weight:
-        #    crt = E[j]
         crt:el = el if (el.w <= max_weight) else El([], 0, 0)
-        #print(crt)
         Ij:list[El] = E[j+1:]
-        # we're not going to store a solution as a list but instead as one element that will contain the sum of each values and weight and the list of indexes
-        
-        #print("crt", crt)
-        Rj:El = greedy_knapsack(deepcopy(Ij), max_weight, crt)
-        
-        #print("")
-        #print(Rj, "\n")
-        #if E[j].w + Rj.w < max_weight : 
-        #  Rj += E[j]
-        R.add(Rj)
-        
-        def pr():
-            print("Ij", "\n".join([str(x) for x in Ij]), "\n")
-            print("Rj", Rj, "\n")
-            print("R","\n".join([str(x) for x in R]), "\n")
-        #pr()
-        if 2 == 3: print("lul")
-    tmp = max(R).idxs
-    #out=[weights[i] for i in tmp][::-1], [values[i] for i in tmp][::-1]
-    return max(R).idxs
-    #return max(R).idxs
+        # we're not going to store a solution as a list but instead as one element that will contain the sum of each values and weight and the list of each summed values&weights
+        Rj:El = greedy_knapsack(Ij, max_weight, crt)        
+        R.append(Rj)
+    
+    return max(R, key=lambda x: x.v).idxs # takes max w.r.t. values
 
-def test_sol(weights, values, sol: tuple):
-    #sol_w = sum([weights[i] for i in sol[0]])
-    #sol_v = sum([values[i] for i in sol[1]])
-    tmp = list(zip(sol[0], sol[1]))
-    return tmp, (sum(sol[0]), sum(sol[1]))
- 
 if __name__ == '__main__':
     w, v = [12, 10, 7, 3, 1], [9, 5, 4, 2, 2]
     print("Res1", approx_knapsack(w, v, 0)) # == ([], [])
