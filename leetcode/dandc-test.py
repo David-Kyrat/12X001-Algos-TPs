3e54ddf1f74cc98484ac295a603e578ccc5be767
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-07-19 19:49:47 +0200

    correct version of divide&conquer with combine

--- leetcode/dandc-test.py
+++ leetcode/dandc-test.py
@@ -1,8 +1,10 @@
 from typing import List
-
+import pytest
 
 # compute sum of array with D&C
 def dc(nums: List[int]) -> int:
+    if not nums or len(nums) <= 0: return 0
+    if len(nums) == 1: return nums[0]
     i = 0
     # return S(J)
     def combine(s_i1, s_i2) -> int:
@@ -29,13 +31,26 @@ def correct_sum(nums: List[int]) -> int:
     max = nums[-1]
     return int(max * (max + 1) / 2)
 
-def test(n: int):
+
+def test_with_five():
+    inp = list(range(5))
+    expected = correct_sum(inp)
+    obtained = dc(inp)
+    assert expected == obtained
+
+def test_for_1_to_100():
+    inputs = [list(range(i)) for i in range(1, 100)]
+    for inp in inputs:
+        assert correct_sum(inp) == dc(inp)
+
+
+""" def __test__(n: int):
     inp = list(range(n+1))
     print("\t", inp)
     expected = correct_sum(inp)
     obtained = dc(inp)
     print(f"expected: {expected}, obtained: {obtained}")
-    
+    assert expected == obtained
 
 if __name__ == "__main__":
-    test(5)
+    __test__(5) """
