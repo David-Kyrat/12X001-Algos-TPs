94dcce1be1753b84a7f3be9156fd8b657c1a5c82
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-07-19 20:24:01 +0200

    correct version of divide&conquer with combine

--- leetcode/dandc-test.py
+++ leetcode/dandc-test.py
@@ -1,33 +1,30 @@
 from typing import List
-import pytest
+
 
 # compute sum of array with D&C
 def dc(nums: List[int]) -> int:
     if not nums or len(nums) <= 0: return 0
     if len(nums) == 1: return nums[0]
-    i = 0
-    # return S(J)
-    def combine(s_i1, s_i2) -> int:
-        return s_i1 + s_i2
+    
+    def combine(s_i1, s_i2) -> int: return s_i1 + s_i2
 
+    i = 0
     def dc_rec(p: int, q: int) -> int:
         nonlocal i
-        if p >= q:
-            assert p == q
-            print(f"nums[{p}] = {nums[p]}\n")
-            return nums[p]
-        else:
-            m = (q + p) // 2
-            print(f"p{i} = {p}  m{i} = {m}  q{i} = {q} {nums[p: m + 1]} | {nums[m+1 : q+1]}")
-            return combine(dc_rec(p, m), dc_rec(m + 1, q))
+        if p >= q: return nums[p] # print(f"nums[{p}] = {nums[p]}\n")
+        m = (q + p) // 2
+        # print(f"p{i} = {p}  m{i} = {m}  q{i} = {q} {nums[p: m + 1]} | {nums[m+1 : q+1]}")
+        return combine(dc_rec(p, m), dc_rec(m + 1, q))
 
     return dc_rec(0, len(nums) - 1)
 
 
 # must be sorted and increased only by 1
 def correct_sum(nums: List[int]) -> int:
-    if not nums or len(nums) < 1: return 0
-    if len(nums) == 1: return nums[0]
+    if not nums or len(nums) < 1:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
     max = nums[-1]
     return int(max * (max + 1) / 2)
 
@@ -38,8 +35,9 @@ def test_with_five():
     obtained = dc(inp)
     assert expected == obtained
 
-def test_for_1_to_100():
-    inputs = [list(range(i)) for i in range(1, 100)]
+
+def test_for_1_to_1000():
+    inputs = [list(range(i)) for i in range(1, 1000)]
     for inp in inputs:
         assert correct_sum(inp) == dc(inp)
 
