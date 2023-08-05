a917dd90c402f8104b2e06b533c5e09078c7c149
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-08-05 21:36:08 +0200

    update

--- leetcode/bb-tp05-shortest-path.py
+++ leetcode/bb-tp05-shortest-path.py
@@ -1,18 +1,23 @@
-from typing import Dict, List, Optional, Set, Self, Tuple
+from typing import Dict, List, Optional, Set, Tuple
 
 # from enum import Enum
 
 
 class Cell:
-    def __init__(self, coords: Tuple[int, int], board: List[List[Self]]) -> None:
+    def __init__(self, coords: Tuple[int, int], board: List[List[int]]) -> None:
         row, col = coords
         self.row = row
         self.col = col
         self.free = board[row][col] == 0
     
     def __repr__(self):
+
         free_str = '✅' if self.free else '❌'
         return f"({self.row}, {self.col} {free_str})"
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Cell): return False
+        return self.row == __value.row and self.col == __value.col
 
 
 class Node:
@@ -26,9 +31,18 @@ class Node:
         """Return: Depth i.e. nb of parent from root up to `self`"""
         return len(self.state)
 
+    @staticmethod
+    def move_to_str(move: Optional[int]) -> str:
+        match move:
+            case 0: return "⬆️"
+            case 3: return "▶️"
+            case 2: return "⬇️"
+            case 1: return "◀️"
+            case _: return ""
+    #NB: in test index on (left, right) axis is reversed hence the swap 3,1
     def __repr__(self):
         #return f"({self.path}, val={self.val} cost={self.cost})"
-        return f"({(self.pos.row, self.pos.col)}, cost={self.cost}, lm={self.last_move})"
+        return f"({(self.pos.row, self.pos.col)}, cost={self.cost}, {Node.move_to_str(self.last_move)})"
 
 
 def up(row: int, col: int): return row - 1, col
@@ -59,7 +73,7 @@ def valid_moves(node: Node, N: int, M: int) -> Set[int]:
     if j <= 0: out.discard(LEFT)
     return out
 
-def listOfChildren(node: Node, board: List[List[Cell]]) -> Set[int]:
+def listOfChildren(node: Node, board: List[List[int]]) -> Set[int]:
     not_out_of_bounds: Set[int] = valid_moves(node, len(board), len(board[0]))
     # remove last move to avoid having 2 consecutive moves cancelling each other
     if node.last_move is not None: not_out_of_bounds.discard(opposite(node.last_move))
@@ -78,10 +92,6 @@ def cost(cell: Cell, depth: int, goal: Cell) -> int:
     l1_norm = abs(crt_row - goal_row) + abs(crt_col - goal_col) # g(x) i.e. distance
     return depth + l1_norm
 
-def _cost(node: Node, goal: Cell) -> int:
-    """ 'Overload' of cost method """
-    return cost(node.pos, node.depth(), goal)
-
 def addToLiveNodes(pq: List[Node], node: Node):
     """Emulate behavior of add PriorityQueue """
     pq.append(node)
@@ -97,21 +107,63 @@ def nextENode(pq: List[Node]) -> Node:
 def P(node: Node, goal: Cell):
     return node.pos == goal
 
-def shortest_path(goal: Cell, board: List[List[Cell]], start: Cell):
-    root = Node(start, 0, [], None)
+def shortest_path(board: List[List[int]], _start: Tuple[int, int], _goal: Tuple[int, int]) -> Node:
+    start, goal = Cell(_start, board), Cell(_goal, board)
     live_nodes = []
+    root = Node(start, 0, [], None)
+    # print("goal  =", goal, "\n")
     enode = root
-    while not P(enode, goal):
-        
-        print("enode =", enode)
+    while not P(enode, goal): #and idx < count:
+        # print("enode =", enode.pos)
         for move in listOfChildren(enode, board):
             new_cell: Cell = Cell(MOVES[move](enode.pos.row, enode.pos.col), board)
             new_node = Node(new_cell, cost(new_cell, enode.depth() + 1, goal), [*enode.state, enode.pos], move)
             addToLiveNodes(live_nodes, new_node)
 
-        print(live_nodes)
+        #print(live_nodes)
+        # print("\n")
         enode = nextENode(live_nodes)
 
+    print("\n\n")
+    return enode
+
+def node_to_path(node: Node) -> List[Tuple[int, int]]:
+    """ return list of pair from root node to given node """
+    out = [(x.row, x.col) for x in node.state]
+    out.append((node.pos.row, node.pos.col))
+    return out
+
+#FREE, BLOCKED = False, True
 
 
-FREE, BLOCKED = False, True
+def test_solve_shortest_path():
+    domain = [
+        # We want to get here
+        # |
+        # v
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        #   ^
+        #   |
+        #   We start from here
+    ]
+    a, b = (7, 6), (0, 7)
+    expected = [a, (7, 7), (6, 7), (5, 7), (4, 7), (3, 7), (2, 7), (1, 7), b]
+    obtained = node_to_path(shortest_path(domain, a, b))
+
+    for cell in obtained: print(cell)
+
+    print("-----")
+    print("expected:")
+    for pair in expected:
+        print(pair)
+    assert obtained == expected
+
+if __name__ == "__mai__":
+    goal = test_solve_shortest_path()
