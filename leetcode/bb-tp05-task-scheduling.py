4509fc2e8b624b29f4425e95d9ab94d1f8d8d563
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-08-07 21:03:22 +0200

    update

--- leetcode/bb-tp05-task-scheduling.py
+++ leetcode/bb-tp05-task-scheduling.py
@@ -1,46 +1,42 @@
 from math import inf
 from typing import List, Tuple
 
+COST_MATRIX = []
 
 class Node:
-    def __init__(self, affectation: Tuple[int, int], cost: int, state: List[Tuple[int, int]]):
+    def __init__(self, cost: int, state: List[Tuple[int, int]]):
         """affectation: task_index, agent_index , state: list of tuple[task_index, agent_index]"""
-        self.affectation = affectation
         self.cost = cost
         self.state = state.copy()
 
-def get_used_agents(node: Node) -> set[int]:
-    """Return: list of agents already used on previous tasks"""
-    return {pairing[1] for pairing in node.state}
-
+    def affectation(self) -> Tuple[int, int]: return self.state[-1]
+    def __repr__(self): return f"({self.state[-1]}, c={self.cost}), cost={COST_MATRIX[self.affectation()[0]][self.affectation()[1]]}"
 
 def cost(affectations: List[Tuple[int, int]], cost_mat: List[List[int]]) -> int:
     h = sum(cost_mat[task_idx][cost_idx] for (task_idx, cost_idx) in affectations)  # cost accumulated up to here
-    used_idx = set()
+    used_idx = {affect[0] for affect in affectations}
     g, k = 0, len(affectations)
     for row in cost_mat[k:]:
         min_idx, min_cost = None, inf  # no minimum yet
         for a_idx, a_cost in enumerate(row):
-            # if a_cost < min_cost and a_idx not in used_idx:
             if a_cost < min_cost and a_idx not in used_idx:
                 used_idx.discard(min_idx)  # agent `min_idx` isn't affected anymore
                 min_idx, min_cost = a_idx, a_cost
                 used_idx.add(a_idx)
-        g = min_cost
-    return g + h
+        g += min_cost
+    return g + h # type: ignore
 
 
 def listOfChildren(parent: Node, cost_mat: List[List[int]]):
-    old_affectations = parent.state + [parent.affectation]
+    old_affectations = parent.state
     used_agents = {affectation[1] for affectation in old_affectations}
     # agent already used in previous tasks
-    N, task_index = len(cost_mat), parent.affectation[0] + 1  # nb of agents & tasks
+    N, task_index = len(cost_mat), parent.affectation()[0] + 1  # nb of agents & tasks
     free_agents = [idx for idx in range(N) if idx not in used_agents]
     return [
         Node(
-            affectation=(task_index, agent_idx),
             cost=cost(old_affectations + [(task_index, agent_idx)], cost_mat),
-            state=old_affectations,
+            state=old_affectations + [(task_index, agent_idx)],
         )
         for agent_idx in free_agents
     ]
@@ -56,24 +52,27 @@ def addToLiveNodes(node: Node, pq: List[Node]):
         else: break  # already sorted
 
 def P(node: Node, cost_mat: List[List[int]]) -> bool:
-    return len(node.state) == len(cost_mat) - 1  # affectations completed?
+    return len(node.state) == len(cost_mat)  # affectations completed?
 
 
 def branch_bound(cost_mat: List[List[int]]) -> Tuple[List[Tuple[int, int]], int]:
     """Return: affectations, i.e. list of coordinates"""
     root_affect = 0, cost_mat[0].index(min(cost_mat[0]))
-    root = Node(root_affect, cost([], cost_mat), [])
-    live_nodes = [root]
+    root = Node(cost([root_affect], cost_mat), [root_affect])
+    live_nodes = []
     enode = root
     while not P(enode, cost_mat):
-        for node in listOfChildren(enode, cost_mat): addToLiveNodes(node, live_nodes)
+        for node in listOfChildren(enode, cost_mat):
+            addToLiveNodes(node, live_nodes)
         enode = live_nodes.pop()
-    return enode.state + [enode.affectation], enode.cost
+        print(enode)
+    return enode.state, enode.cost
 
 
 if __name__ == "__main__":
     # example : cost of task 1 by agent 2 is 17 ( numbering starts at index 0)
-    COST_MATRIX = [[11, 14, 11, 17, 24], [12, 15, 17, 14, 35], [18, 13, 19, 20, 27], [40, 22, 23, 28, 18], [12, 43, 25, 34, 55]]
+    # COST_MATRIX = [[11, 14, 11, 17, 24], [12, 15, 17, 14, 35], [18, 13, 19, 20, 27], [40, 22, 23, 28, 18], [12, 43, 25, 34, 55]]
+    COST_MATRIX = [ [11 , 14 , 11 , 17] , [12 , 15 , 17 , 14] , [18 , 13 , 19 , 20] , [40 , 22 , 23 , 28] ]
     result, cost_tot = branch_bound(COST_MATRIX)
     min_mat = 0
     for row in COST_MATRIX: min_mat += min(row)
@@ -81,7 +80,6 @@ if __name__ == "__main__":
 
     print("total cost:", cost_tot)
     for task_idx, agent_idx in result:
-        # print(f"task{task_idx} - agent{agent_idx}: ", COST_MATRIX[task_idx][agent_idx])
         print(f"agent{agent_idx} - task{task_idx}", COST_MATRIX[task_idx][agent_idx])
     # the optimal assignement for this cost matrix is :
     # agent0 - task0 , agent1 - task2 , agent2 - task3 , agent3 - task1 .
--- leetcode/fifteen-puzzle.py
+++ leetcode/fifteen-puzzle.py
@@ -1,3 +1,19 @@
-from typing import List, Set, Tuple, Optional, Self
+from typing import List, Optional, Self, Set, Tuple
 
 
+def cost(_board: List[List[int]], state: List[int], start_pos: Tuple[int, int] ) -> int:
+    """state: List of moves to apply to get the board from which to compute the cost
+            NB: must include last move. i.e. call this function like this: `cost(..., parent_state + [crt_move], ...)`
+    start_pos: position of blank space for root node"""
+    return 0
+
+class Node:
+    def __init__(self, move: int, bpos: Tuple[int, int], state: List[int], board: List[List[int|None]], start_pos: Tuple[int, int]) -> None:
+        """bpos: position of blank space,
+        state: list of moves (a move is an int in [0, 3])"""
+        self.move = move
+        self.bpos = bpos
+        self.state = state
+        self.cost = cost(board, state + [move], )
+
+    
